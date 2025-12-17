2ebea09b09b25c78ab61a8a07aa9a59cbdc4a039
Prak005 <prakharmallick005@gmail.com>
2025-12-17 23:20:06 +0530

    add display order

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import GeneralInfo from './components/GeneralInfo';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import './styles/App.css';
+
+function App(){
+    return(
+        <div className="App">
+            <h1>CV Generator</h1>
+            <GeneralInfo />
+            <Education />
+            <Experience />
+        </div>
+    );
+}
+
+export default App;
