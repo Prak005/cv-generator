1ea3c04ba6de8ce12f0e72f848c8e9bc605d7ba3
Prak005 <prakharmallick005@gmail.com>
2025-12-20 23:55:05 +0530

    feat: add user image

---
+++ src/components/UserImg.jsx
@@ -0,0 +1,22 @@
+import React, {useState, useRef} from 'react';
+
+function UserImg() {
+    const [image, setImage] = useState('./assets/profile.svg');
+    const fileInputRef = useRef(null);
+
+    function handleImageSelect(e){
+        const file = e.target.files[0];
+        if(file){
+            setImage(URL.createObjectURL(file));
+        }
+    }
+
+    return (
+        <div className="user-img">
+            <img src={image} alt="user" onClick={() => fileInputRef.current.click()} style={{cursor: 'pointer'}} />
+            <input type="file" accept="image/*" ref={fileInputRef} onChange={handleImageSelect} hidden />
+        </div>
+    );
+}
+
+export default UserImg;
