3989887f6a02061d73bf9a4ed1b95082928941d5
Prak005 <prakharmallick005@gmail.com>
2025-12-20 23:54:13 +0530

    feat: education info

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,28 +1,32 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-function Education() {
+function Education(){
     const [submitted, setSubmitted] = useState(false);
-    const [edu, setEdu] = useState({school: '', title: '', date: ''});
-
+    const [edu, setEdu] = useState({from:'', to:'', name:'', course:'', gpa:''});
     if(submitted){
-        return(
-            <div className="section-result">
+        return (
+            <div className="education">
                 <h3>Education</h3>
-                <p><strong>{edu.school}</strong> - {edu.title}</p>
-                <p>Graduated: {edu.date}</p>
+                <p><strong>{edu.from} - {edu.to}</strong></p>
+                <p><strong>{edu.name}</strong></p>
+                <ul>
+                    <li>{edu.course}</li>
+                    <li>{edu.gpa}/4.0</li>
+                </ul>
                 <button className="edit-btn" onClick={() => setSubmitted(false)}>Edit Education</button>
             </div>
         );
     }
+
     return(
-        <form className="cv-creation" onSubmit={() => setSubmitted(true)}>
+        <form className="education" onSubmit={() => setSubmitted(true)}>
             <h3>Education</h3>
-            <input type="text" placeholder="School Name" value={edu.school} onChange={(e) => setEdu({...edu, school:e.target.value})}/>
-            <input type="text" placeholder="Title of Study" value={edu.title} onChange={(e) => setEdu({...edu, title:e.target.value})}/>
-            <input type="text" placeholder="Date of Study" value={edu.date} onChange={(e) => setEdu({...edu, date:e.target.value})}/>
-            <button type="submit">Save</button>
+            <input type="number" placeholder="Start Year" value={edu.from} onChange={(e) => setEdu({...edu, from:e.target.value})}/>
+            <input type="number" placeholder="End Year" value={edu.to} onChange={(e) => setEdu({...edu, to:e.target.value})}/>
+            <input type="text" placeholder="Institute Name" value={edu.name} onChange={(e) => setEdu({...edu, name: e.target.vaue})}/>
+            <input type="number" placeholder="Enter GPA" value={edu.gpa} onChange={(e) => setEdu({...edu, gpa:e.target.value})}/>
         </form>
-    );
+    )
 }
 
 export default Education;
