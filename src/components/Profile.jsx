79bf556845c39004710686d1782b84970b63ddb8
Prak005 <prakharmallick005@gmail.com>
2025-12-24 12:12:02 +0530

    change rendering

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,22 +2,22 @@ import React, {useState} from 'react';
 
 function Profile() {
     const [submitted, setSubmitted] = useState(false);
-    const [profile, setProfile] = useState({text:''});
+    const [profile, setProfile] = useState('');
 
     if(submitted){
         return(
             <div className="profile">
                 <h3>Profile</h3>
-                <p>{profile.text}</p>
+                <p>{profile}</p>
                 <button className="edit-btn" onClick={() => setSubmitted(false)}>Edit Profile</button>
             </div>
         );
     }
 
     return(
-        <form className="profile" onSubmit={() => setSubmitted(true)}>
+        <form className="profile" onSubmit={(e) => {setSubmitted(true); e.preventDefault();}}>
             <h3>Profile</h3>
-            <input type="text" value={profile.text} onChange={(e) => setProfile({text:e.target.value})}/>
+            <textarea rows={5} value={profile} onChange={(e) => setProfile(e.target.value)}/>
             <button type="submit">Save</button>
         </form>
     );
